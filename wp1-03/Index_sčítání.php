eb539217a86f059b5d36bca3620bda19013c8556
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-03-03 08:32:11 +0100

    Update Index_sčítání.php

--- wp1-03/Index_sčítání.php
+++ wp1-03/Index_sčítání.php
@@ -1,6 +1,18 @@
 <?php
-$value1 = 10;
-$value2 = 5;
+$value1 = "20";
+$value2 = "0";
+
+$summary = $a + $b
+$difference = $a - $b
+$product = $a * $b
+
+if ($b != 0) {
+    $portion = $a / $b
+}
+else {
+    $portion = "nelze dělit nulou"
+}
+
 ?>
 
 
@@ -18,12 +30,12 @@ $value2 = 5;
 <p> Proměnná A </p> <?= $value1; ?>
 <p> Proměnná B </p> <?= $value2; ?>
 <h3> Součet </h3>
-<?= $value1; ?> + <?= $value2; ?> = <?= $value1 + $value2; ?>
+<?= $value1; ?> + <?= $value2; ?> = <?= $summary; ?>
 <h3> Rozdíl </h3>
-<?= $value1; ?> - <?= $value2; ?> = <?= $value1 - $value2; ?>
+<?= $value1; ?> - <?= $value2; ?> = <?= $difference; ?>
 <h3> Součin </h3>
-<?= $value1; ?> * <?= $value2; ?> = <?= $value1 * $value2; ?>
+<?= $value1; ?> * <?= $value2; ?> = <?= $product; ?>
 <h3> Podíl </h3>
-<?= $value1; ?> / <?= $value2; ?> = <?= $value1 / $value2; ?>
+<?= $value1; ?> / <?= $value2; ?> = <?= $difference; ?>
 </body>
 </html>
